cd18abb351ed04c40373ee37ce2800518b2086d8
Mario Henrique Souto <mariohsouto@gmail.com>
2016-02-07 18:15:52 -0200

    GM22

--- src/GramMatrix.jl
+++ src/GramMatrix.jl
@@ -9,11 +9,17 @@
 #to complete
 
 GM = Matrix{Function}(2,2)
-GM[1,2]=GM12
-GM[2,1]=GM12
+GM[1, 1] = GM11
+GM[1, 2] = GM12
+GM[2, 1] = GM12
+GM[1, 3] = GM13
+GM[3, 1] = GM13
+GM[1, 4] = GM14
+GM[4, 1] = GM14
+GM[1, 5] = GM15
+GM[5, 1] = GM15
 
-function GM11(i::Int,j::Int,L::Vector{Float64},U::Vector{Float64},N::Int,
-              μ0::Float64,σ0::Float64)
+function GM11(i::Int, j::Int, N::Int, μ::Vector{Float64}, σ::Vector{Float64})
   # step x step
   GM = Float64[]
   m = min(i, j)
@@ -26,63 +32,81 @@ function GM11(i::Int,j::Int,L::Vector{Float64},U::Vector{Float64},N::Int,
   return GM
 end
 
-function GM12(i::Int,j::Int,N::Int,μp::Vector{Float64},σp::Vector{Float64},
-              μs::Vector{Float64},σs::Vector{Float64})
+function GM12(i::Int, j::Int, N::Int, μ::Vector{Float64}, σ::Vector{Float64})
   # step x slope
   GM = Float64[]
 
   if i > j
     GM = (
-      j*μ[j]*μ[i]-(i-j)*(1-μ[j])*μ[i]-μ[i]*(N+1)+μ[i]*μ[j]*(N+1)
-      + 0.5*(N+1)^2-0.5*N-0.5*μ[j]*(N+1)^2+0.5*μ[j]*(N+1)
-      + μ[i]*(i+1)-μ[i]*μ[j]*(i+1)-0.5*(i+1)^2+0.5*i
-      + 0.5*μ[j]*(i+1)^2-0.5*μ[j]*(i+1)
+      j*μ[j]*μ[i] - (i-j)*(1-μ[j])*μ[i] - μ[i]*(N+1) + μ[i]*μ[j]*(N+1)
+      + 0.5*(N+1)^2 - 0.5*N - 0.5*μ[j]*(N+1)^2 + 0.5*μ[j]*(N+1)
+      + μ[i]*(i+1) - μ[i]*μ[j]*(i+1) - 0.5*(i+1)^2 + 0.5*i
+      + 0.5*μ[j]*(i+1)^2 - 0.5*μ[j]*(i+1)
       )/(σ[i]*σ[j])
   else
     GM = (
-      i*μ[i]*μ[j]-(i+1)*μ[i]*μ[j]+0.5*μ[j]*(i+1)^2-0.5*μ[j]*(i+1)
-      - μ[i]*(N+1)+μ[i]*μ[j]*(N+1)+0.5*(N+1)^2-0.5*N-0.5*μ[j]*(N+1)^2
-      + 0.5*μ[j]*(N+1)+μ[i]*(j+1)-0.5*(j+1)^2+0.5*j
+      i*μ[i]*μ[j] - (i+1)*μ[i]*μ[j] + 0.5*μ[j]*(i+1)^2 - 0.5*μ[j]*(i+1)
+      - μ[i]*(N+1) + μ[i]*μ[j]*(N+1) + 0.5*(N+1)^2 - 0.5*N-0.5*μ[j]*(N+1)^2
+      + 0.5*μ[j]*(N+1) + μ[i]*(j+1) - 0.5*(j+1)^2 + 0.5*j
       )/(σ[i]*σ[j])
   end
 
   return GM
 end
 
-function GM13(i::Int,j::Int,N::Int,μp::Vector{Float64},σp::Vector{Float64},
-              μs::Vector{Float64},σs::Vector{Float64})
+function GM13(i::Int, j::Int, N::Int, μ::Vector{Float64}, σ::Vector{Float64})
   # step x spike
   GM = Float64[]
 
   if i > j
     GM = (
-      j*μ[j]*μ[i]-(N-j)*(1-μ[j])*μ[i]+(1-μ[j])*μ[i]+(1-μ[j])*(1-μ[i])
+      j*μ[j]*μ[i] - (N-j)*(1-μ[j])*μ[i] + (1-μ[j])*μ[i] + (1-μ[j])*(1-μ[i])
       )/(σ[i]*σ[j])
   else
     GM = (
-      j*μ[j]*μ[i]-μ[i]*μ[j]-μ[j]*(1-μ[i])-(N-j)*(1-μ[j])*μ[i]
+      j*μ[j]*μ[i] - μ[i]*μ[j] - μ[j]*(1-μ[i]) - (N-j)*(1-μ[j])*μ[i]
       )/(σ[i]*σ[j])
   end
 
   return GM
 end
 
-function GM22(i::Int,j::Int,N::Int,μ::Vector{Float64},σ::Vector{Float64})
+function GM14(i::Int, j::Int, N::Int, μ::Vector{Float64}, σ::Vector{Float64})
+  # step x sine
+  GM = Float64[]
 
+  GM = (
+  )/(σ[i]*σ[j])
 
-    GM = Float64[]
-    ab = convert(Float64,abs(i-j))
-    m = convert(Float64,minimum(i,j))
-    M = convert(Float64,maximum(i,j))
-
-    GM = (μ[i]*μ[j]*(m-1.0) + μ[i]*μ[j]*ab
-            +(-μ[M])*ab*(ab+1.0)/2.0
-            +(N-M+1.0)*(-μ[m]+ab)*(-μ[M])
-            +(-μ[i]-μ[j]+ab)*(N-M+1.0)*(N-M+2.0)/2.0
-            +(N-M+1.0)*(N-M+2.0)*(2.0N-2.0M+3.0)/6.0
-            )/σ[i]/σ[j]
-    return GM
+  return GM
 end
 
+function GM15(i::Int, j::Int, N::Int, μ::Vector{Float64}, σ::Vector{Float64})
+  # step x cosine
+  GM = Float64[]
+
+  GM = (
+  )/(σ[i]*σ[j])
+
+  return GM
+end
+
+function GM22(i::Int, j::Int, N::Int, μ::Vector{Float64}, σ::Vector{Float64})
+  # slope x slope
+  GM = Float64[]
+  m = min(i, j)
+  M = max(i, j)
+
+  GM = (
+    (1/6)*N + m*μ[m]*μ[M] - (1/6)*M-0.5*(N+1)^2 + (1/3)*(N+1)^3 + 0.5*μ[M]*(m+1)^2
+    - 0.5*μ[M]*(m+1) - μ[M]*(m+1)*m - μ[M]*μ[m]*(m+1) + 0.5*(M+1)^2 - (1/3)*(M+1)^3
+    + 0.5*m*(M+1)^2 - 0.5*m(M+1) + 0.5*μ[m]*(M+1)^2 - 0.5*μ[m]*(M+1) + 0.5*M*(M+1)^2
+    - 0.5*M*(M+1) - 0.5*m*(N+1)^2 + 0.5*m*(N+1) - 0.5*μ[m]*(N+1)^2 + 0.5*μ[m]*(N+1)
+    -0.5*M*(N+1)^2 + 0.5*M*(N+1) - 0.5*μ[M]*(N+1)^2 + 0.5*μ[M]*(N+1) - (M+1)*n*M
+    - (M+1)*μ[m]*M + (N+1)*m*M + μ[M]*(N+1)*m
+  )/(σ[i]*σ[j])
+
+  return GM
+end
 
 
