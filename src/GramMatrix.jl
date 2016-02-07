1ad6b2a095e1185e0abfd2e65af88eb8d617caa4
Mario Henrique Souto <mariohsouto@gmail.com>
2016-02-07 15:33:53 -0200

    GM12

--- src/GramMatrix.jl
+++ src/GramMatrix.jl
@@ -14,15 +14,42 @@ GM[2,1]=GM12
 
 function GM11(i::Int,j::Int,L::Vector{Float64},U::Vector{Float64},N::Int,
               μ0::Float64,σ0::Float64)
+  # step x step
+  GM = Float64[]
+  m = min(i, j)
+  M = max(i, j)
 
-    GM = Float64[]
-    m = min(i, j)
-    M = max(i, j)
+  GM = (
+    m*μ[m]*μ[M]-(M-m)*(1-μ[m])*μ[M]+(N-M)*(1-μ[m])*(1-μ[M])
+    )/(σ[m]*σ[M])
 
-    GM = (m*μ[m]*μ[M]-(M-m)*(1-μ[m])*μ[M]+(N-M)*(1-μ[m])*(1-μ[M]))/(σ[m]*σ[M])
-
-    return GM
+  return GM
 end
+
+function GM12(i::Int,j::Int,N::Int,μp::Vector{Float64},σp::Vector{Float64},
+              μs::Vector{Float64},σs::Vector{Float64})
+  # step x slope
+  GM = Float64[]
+
+  if i > j
+    GM = (
+      j*μ[j]*μ[i]-(i-j)*(1-μ[j])*μ[i]-μ[i]*(N+1)+μ[i]*μ[j]*(N+1)
+      + 0.5*(N+1)^2-0.5*N-0.5*μ[j]*(N+1)^2+0.5*μ[j]*(N+1)
+      + μ[i]*(i+1)-μ[i]*μ[j]*(i+1)-0.5*(i+1)^2+0.5*i
+      + 0.5*μ[j]*(i+1)^2-0.5*μ[j]*(i+1)
+      )/(σ[i]*σ[j])
+  else
+    GM = (
+      i*μ[i]*μ[j]-(i+1)*μ[i]*μ[j]+0.5*μ[j]*(i+1)^2-0.5*μ[j]*(i+1)
+      - μ[i]*(N+1)+μ[i]*μ[j]*(N+1)+0.5*(N+1)^2-0.5*N-0.5*μ[j]*(N+1)^2
+      + 0.5*μ[j]*(N+1)+μ[i]*(j+1)-0.5*(j+1)^2+0.5*j
+      )/(σ[i]*σ[j])
+  end
+
+  return GM
+end
+
+function GM12T(i,j,N,μp,σp,μs,σs) = GM12(j,i,N,μp,σp,μs,σs)
 
 function GM22(i::Int,j::Int,N::Int,μ::Vector{Float64},σ::Vector{Float64})
 
@@ -39,28 +66,6 @@ function GM22(i::Int,j::Int,N::Int,μ::Vector{Float64},σ::Vector{Float64})
             )/σ[i]/σ[j]
     return GM
 end
-function GM12(i::Int,j::Int,N::Int,μp::Vector{Float64},σp::Vector{Float64},
-    μs::Vector{Float64},σs::Vector{Float64})
 
-    GM = Float64[]
-
-    if i > j
-        GM = (μp[i]*μs[j]*((i-1.0)+(j-i))
-        + (-μs[j]*(j-i)*(j-i+1.0)/2.0)
-        + (1.0-μs[j])*(j-i-μp[i])*(N-j+1.0)
-        + (1.0-μs[j])*(N-j+1.0)*(N-j+2.0)/2.0
-        )/σp[i]/σs[j]
-    else
-        GM = (μp[i]*μs[j]*((j-1.0)+(i-j))
-        + (-μs[j]*(1.0-μs[j])*(i-j)/2.0)
-        + (1.0-μs[j]+(i-j))*(-μp[i])*(N-i+1.0)
-        + (1.0-μs[j]+(i-j))*(N-i+1.0)*(N-i+2.0)/2.0
-        )/σp[i]/σs[j]
-    end
-
-    return GM
-end
-
-function GM12T(i,j,N,μp,σp,μs,σs) = GM12(j,i,N,μp,σp,μs,σs)
 
 
