d5dcedd7fdc8999ff4fdc3a756529603eec197cd
Mario Henrique Souto <mariohsouto@gmail.com>
2016-02-18 02:26:31 -0200

    fixed normalization

--- src/Normalization.jl
+++ src/Normalization.jl
@@ -1,94 +1,17 @@
+# computes μ (mean) and σ (standard deviation) for each kind of component
 
-
-function getCosData(IT,f)
-    N = IT.obs
-
-    nf = IT.nelements[5]
-
-    μ = Vector{Float64}(nf)
-    σ = Vector{Float64}(nf)
-    sf=0.0
-    cf=0.0
-    sn1f=0.0
-    cn1f=0.0
-
-
-    for i = 1:nf
-        sf = sin(f[i])
-        cf = cos(f[i])
-        sn1f = sin((N+1)*f[i])
-        cn1f = cos((N+1)*f[i])
-
-
-        μ[i] =  -1/2*cn1f
-                -1/2*(sf*sn1f/(cf-1.0))
-                +1/2*cf
-                +1/2*sf^2/(cf-1.0)
-
-        σ[i] = (1/2 + μ[i]^2)*(N+1) + 1/2*cn1f^2
-               -1/2*cf*sn1f*cn1f/sf - μ[i]*sf*cn1f/(cf-1.0)
-               +sn1f*μ[i] - 1/2 - μ[i]^2 +μ[i]*sf*cf/(cf-1.0) - μ[i]*sf
-
-    end
-
-    return σ,μ
+function getStepData(IT,f)
+    getStepData(IT)
 end
-
-
-function getSineData(IT,f)
+function getStepData(IT)
     N = IT.obs
-
-    nf = IT.nelements[4]
-
-
-    μ = Vector{Float64}(nf)
-    σ = Vector{Float64}(nf)
-    sf=0.0
-    cf=0.0
-    sn1f=0.0
-    cn1f=0.0
-
-
-    for i = 1:nf
-        sf = sin(f[i])
-        cf = cos(f[i])
-        sn1f = sin((N+1)*f[i])
-        cn1f = cos((N+1)*f[i])
-
-
-        μ[i] =  1/2*(sf*cn1f/(cf-1))
-               -1/2*sn1f
-               -1/2*sf*cf/(cf-1)
-               +1/2*sf
-
-        σ[i] = (1/2 + μ[i]^2)*(N+1) - 1/2*cn1f^2
-               +1/2*cf*sn1f*cn1f/sf + cn1f*μ[i]
-               +μ[i]*sf*sn1f/(cf-1.0)
-               - 1/2 - μ[i]^2 -cf*μ[i] -μ[i]*sf^2/(cf-1.0)
-
-    end
-
-    return σ,μ
-end
-
-
-function getSpikeData(IT,f)
-    getSpikeData(IT)
-end
-function getSpikeData(IT)
-
-    N = IT.obs
-
     μ = Vector{Float64}(N)
     σ = Vector{Float64}(N)
-    
-    #revisar!!!!
+
     for i = 1:N
-        μ[i] = 1/N
-        σ[i] = (N-1.0)*μ[i] + (1.0-μ[i])^2 
-
+        μ[i] = i/N
+        σ[i] = i*μ[i]^2+(N-1)*(1-μ[i])^2
     end
-
     return σ,μ
 end
 
@@ -96,47 +19,99 @@ function getSlopeData(IT,f)
     getSlopeData(IT)
 end
 function getSlopeData(IT)
-
     N = IT.obs
-
     μ = Vector{Float64}(N)
     σ = Vector{Float64}(N)
-    
-    #revisar!!!! -- Passar as expressões do arquivo "APorraToda.pdf".
+
     for i = 1:N
-        μ[i] = 1
-        σ[i] = 1
+        μ[i] = (
+      0.5*(N+1)^2 - 0.5*N - i*(N+1) - 0.5*(i+1)^2 + 0.5*i + i*(i+1)
+      )/N
 
+        σ[i] = (
+      i*μ[i]^2 + (N+1)*i^2 + 2*(N+1)*i*μ[i] + (N+1)*μ[i]^2 - i*(N+1)^2
+      + i*(N+1) - μ[i]*(N+1)^2 + μ[i]*(N+1) + (1/3)*(N+1)^3
+      - 0.5*(N+1)^2 + (1/6)*N - (i+1)*i^2 - 2*(i+1)*i*μ[i]
+      - (i+1)*μ[i]^2 + i*(i+1)^2 - i*(i+1) + μ[i]*(i+1)^2
+      - μ[i]*(i+1) - (1/3)*(i+1)^3 + 0.5*(i+1)^2 - (1/6)*i
+      )
     end
-
     return σ,μ
 end
 
-function getStepData(IT,f)
-    getStepData(IT) 
+function getSpikeData(IT,f)
+    getSpikeData(IT)
 end
-function getStepData(IT)
-
+function getSpikeData(IT)
     N = IT.obs
+    μ = Vector{Float64}(N)
+    σ = Vector{Float64}(N)
 
-    μ = Vector{Float64}(N-1)
-    σ = Vector{Float64}(N-1)
-    #L = Vector{Float64}(N-1)#remove
-    #U = Vector{Float64}(N-1)
-
-    for i = 1:(N-1)
-        μ[i] = (N-i)/N
-        σ[i] = sqrt( ( i*μ[i]^2.0+(N-i)*(1.0-μ[i])^2.0 )/N )
-        #U[i] = -μ[i]/σ[i]
-        #L[i] = (1.0-μ[i])/σ[i]
+    for i = 1:N
+        μ[i] = i/N
+        σ[i] = (N-1)*μ[i]^2 + (1-μ[i])^2
     end
-
-    return σ,μ#U,L,
+    return σ,μ
 end
 
+function getSineData(IT,f)
+    getSineData(IT, f)
+end
+function getSineData(IT, ω)
+    N = IT.obs
+    N_frequencies = IT.nelements[4]
+    μ = Vector{Float64}(nf)
+    σ = Vector{Float64}(nf)
+
+    for i = 1:N_frequencies
+        sin_ω = sin(ω[i])
+        cos_ω = cos(ω[i])
+        sin_N1ω = sin((N+1)*ω[i])
+        cos_N1ω = cos((N+1)*ω[i])
+
+        μ[i] =  (
+      (sin_ω*cos_N1ω)/(cos_ω-1) - sin_N1ω - (sin_ω*cos_ω)/(cos_ω-1) + sin_ω
+      )/2
+
+        σ[i] = (
+      - μ[i]*sin_ω + (0.5+μ[i]^2)*(N+1) + 0.5*cos_N1ω^2
+      - 0.5*(cos_ω*sin_N1ω*cos_N1ω)/sin_ω - (μ[i]*sin_ω*cos_N1ω)/(cos_ω-1)
+      + sin_N1ω*μ[i] - 0.5 - μ[i]^2 + (μ[i]*sin_ω*cos_ω)/(cos_ω-1)
+      )
+    end
+    return σ,μ
+end
+
+function getCosData(IT,f)
+    getCosData(IT, f)
+end
+function getCosData(IT, ω)
+    N = IT.obs
+    N_frequencies = IT.nelements[5]
+    μ = Vector{Float64}(nf)
+    σ = Vector{Float64}(nf)
+
+    for i = 1:nf
+        sin_ω = sin(ω[i])
+        cos_ω = cos(ω[i])
+        sin_N1ω = sin((N+1)*ω[i])
+        cos_N1ω = cos((N+1)*ω[i])
+
+        μ[i] =  (
+      - 0.5*cos_N1ω - 0.5*(sin_ω*sin_N1ω)/(cos_ω-1) + 0.5*cos_ω
+      + 0.5*sin_ω^2/(cos_ω-1)
+      )
+
+        σ[i] = (
+      (0.5+μ[i]^2)*(N+1) - 0.5*cos_N1ω^2 - (μ[i]*sin_ω^2)/(cos_ω-1)
+      + 0.5*(cos_ω*sin_N1ω*cos_N1ω)/sin_ω + cos_N1ω*μ[i]
+      + (μ[i]*sin_ω*sin_N1ω)/(cos_ω-1) -0.5 -μ[i]^2 - cos_ω*μ[i]
+      )
+    end
+    return σ,μ
+end
 
 getData = Vector{Function}(5)
-
 getData[STEP] = getStepData
 getData[SLOPE] = getSlopeData
 getData[SPIKE] = getSpikeData
--- src/initializations.jl
+++ src/initializations.jl
@@ -16,19 +16,19 @@ function initIT_range(N,components, f = Vector{Float64}(0) )
 	end
 	if SLOPE in components
 		#push!(components,SLOPE)
-		if STEP in components 
-			nelements[SLOPE] = N-1 
+		if STEP in components
+			nelements[SLOPE] = N-1
 			elements[SLOPE] = 1:(N-1)
-		else 
-			nelements[SLOPE] = N 
+		else
+			nelements[SLOPE] = N
 			elements[SLOPE] = 1:N
 		end
 	end
 	if SPIKE in components
 		#push!(components,SPIKE)
-		if ( STEP in components || SLOPE in components ) 
+		if ( STEP in components || SLOPE in components )
 			nelements[SPIKE] = N-1
-			elements[SPIKE] = 1:(N-1) 
+			elements[SPIKE] = 1:(N-1)
 		else
 			nelements[SPIKE] = N
 			elements[SPIKE] = 1:N
@@ -38,7 +38,7 @@ function initIT_range(N,components, f = Vector{Float64}(0) )
 		#push!(components,SIN)
 		nelements[SIN] = size(f)[1]
 		elements[SIN] = 1:size(f)[1]
-	end	
+	end
 	if COS in components
 		#push!(components,COS)
 		nelements[COS] = size(f)[1]
@@ -52,28 +52,28 @@ end
 
 function initSparse(IT)
 	BCD = Array{SparseMatrixCSC{Float64,Int},1}[]
-	
+
 	beta_tilde = SparseMatrixCSC{Float64,Int}[]
 	beta = SparseMatrixCSC{Float64,Int}[]
-	
+
 	activeSet = SparseMatrixCSC{Bool,Int}[]
-	
+
 	for i in 1:5
-	
+
 		if i in IT.components
-	
+
 			push!(beta_tilde,spzeros(IT.nelements[i],1))
 			push!(beta,spzeros(IT.nelements[i],1))
 			push!(activeSet,spzeros(Bool,IT.nelements[i],1))
-	
+
 		else
-	
+
 			push!(beta_tilde,spzeros(0,0))
 			push!(beta,spzeros(0,0))
 			push!(activeSet,spzeros(Bool,0,0))
-	
+
 		end
-	
+
 	end
 
 	return BCD,beta_tilde,beta,activeSet
@@ -81,35 +81,35 @@ end
 
 function initDense(IT)
 	BCD = Array{Vector{Float64},1}[]
-	
+
 	beta_tilde = Vector{Float64}[]
 	beta = Vector{Float64}[]
-	
+
 	activeSet = Vector{Bool}[]
-	
+
 	for i in 1:5
-	
+
 		if i in IT.components
-	
+
 			push!(beta_tilde,zeros(IT.nelements[i]))
 			push!(beta,zeros(IT.nelements[i]))
 			push!(activeSet,zeros(Bool,IT.nelements[i]))
-	
+
 		else
-	
+
 			push!(beta_tilde,zeros(0))
 			push!(beta,zeros(0))
 			push!(activeSet,zeros(Bool,0))
-	
+
 		end
-	
+
 	end
 
 	return BCD,beta_tilde,beta,activeSet
 end
 
 function initXDY(IT,y,data)
-	
+
 	xdy0 = Vector{Float64}[]
 
 	for i in 1:TOTALCOMPONENTS
