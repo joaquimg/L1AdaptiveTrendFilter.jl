528911c52e54b9e314e079fbfe630cf75c21218d
Mario Henrique Souto <mariohsouto@gmail.com>
2016-02-21 22:35:59 -0300

    removing active component from partial fit computation

--- src/CDmainAlg.jl
+++ src/CDmainAlg.jl
@@ -23,73 +23,77 @@ function CD(y,components; f = Vector{Float64}(0))
     return BCD
 end
 
+# coordinate descent for the whole regularization path Λ
 function CoordinateDescent(IT, d, xdy, Λ; sparse=0)
 
-    if sparse == 1
-        BCD, β_tilde, β, activeSet = initSparse(IT)
-    else
-        BCD, β_tilde, β, activeSet = initDense(IT)
+  if sparse == 1
+    BCD, β_tilde, β, activeSet = initSparse(IT)
+  else
+    BCD, β_tilde, β, activeSet = initDense(IT)
+  end
+
+  BIC = Inf::Float64
+  β_ols = 0.0 :: Float64
+  temp = 0.0 :: Float64
+  tolerance = 1e77
+
+  # regularization path
+  for λ in Λ
+
+    change = true
+    # loop until active set converges
+    for iter in 1:IT.maxIter
+      if !change
+        break
+      end
+
+      # cycle through every component
+      for c1 in IT.components
+        for j in IT.elements[c1]
+
+          # compute the partial fit with the component in the active set
+          partial_fit = 0.0
+          for c2 in IT.components
+            for l in 1:size(activeSet[c2])[1]
+              if activeSet[c2][l] && (c1, j) != (c2, l)
+                partial_fit = partial_fit + GM[c1,c2](j,l,d,IT) * β_tilde[c2][l]
+              end
+            end
+          end
+
+          # univariate ordinary leasts squares coefficient
+          β_ols = β_tilde[c1][j] + (1.0/IT.obs) * (xdy[c1][j] - partial_fit)
+
+          # soft thresholding operator
+          if abs(β_ols) <= λ
+            if activeSet[c1][j]
+              β_tilde[c1][j] = 0.0 #talvez nem precise
+              activeSet[c1][j] = false
+              change = true
+            end
+          else
+            β_tilde[c1][j] = sign(β_ols) * (abs(β_ols) - λ)
+
+            if !activeSet[c1][j]
+              activeSet[c1][j] = true
+              change = true
+            end
+          end
+        end
+      end
     end
 
-    BIC = Inf::Float64
-    β_ols = 0.0 :: Float64
-    temp = 0.0 :: Float64
-    tolerance = 1e77
-
-    for λ in Λ # regularization path
-
-        change = true
-        for iter in 1:IT.maxIter #Main outer LOOP: fix maximum
-            if !change
-                break
-            end
-
-            # cycle through every component
-            for c1 in IT.components
-                for j in IT.elements[c1]
-
-                    temp = 0.0
-                    #GramMatrix LOOP (double loop: component and element)
-                    for c2 in IT.components
-                        for l in 1:size(activeSet[c2])[1]
-                            if activeSet[c2][l]
-                                temp = temp + GM[c1,c2](j,l,d,IT) * β_tilde[c2][l]
-                            end
-                        end
-                    end
-
-                    # univariate ordinary leasts squares coefficient
-                    β_ols = β_tilde[c1][j] + (1.0/IT.obs) * (xdy[c1][j] - temp)
-
-                    # soft thresholding operator
-                    if abs(β_ols) <= λ
-                        if activeSet[c1][j]
-                            β_tilde[c1][j] = 0.0 #talvez nem precise
-                            activeSet[c1][j] = false
-                            change = true
-                        end
-                    else
-                        β_tilde[c1][j] = sign(β_ols) * (abs(β_ols) - λ)
-
-                        if !activeSet[c1][j]
-                            activeSet[c1][j] = true
-                            change = true
-                        end
-                    end
-                end
-            end
-        end
-
-        βtemp = copy(β_tilde)
-        println(βtemp)
-        #push!(BCD,copy(β_tilde))
-        #println(BCD)
-        BIC_new = compute_BIC(y, βtemp, IT, d)
-        if BIC_new < BIC
-            BIC = BIC_new
-            β = βtemp
-        end
+    # bayesian information criterion
+    βtemp = copy(β_tilde)
+    println(βtemp)
+    #push!(BCD,copy(β_tilde))
+    #println(BCD)
+    BIC_new = compute_BIC(y, βtemp, IT, d)
+    if BIC_new < BIC
+      BIC = BIC_new
+      β = βtemp
     end
+  end
 
-    return BCD,β
+  return BCD,β
 end
