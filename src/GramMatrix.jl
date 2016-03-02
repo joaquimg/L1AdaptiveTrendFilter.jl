2e7b598031754f8bd8b9d5ee4de0de691fbeaf53
Mario Henrique Souto <mariohsouto@gmail.com>
2016-03-01 23:20:10 -0300

    removed division from Gram matrix

--- src/GramMatrix.jl
+++ src/GramMatrix.jl
@@ -28,7 +28,7 @@ function GMStepSlope(
       - (1/2)*μSTEP*(N+1)^2 + (1/2)*μSTEP*(N+1) + (l+1)*l+μSLOPE*(l+1)
       - (l+1)*l*μSTEP - μSLOPE*(l+1)*μSTEP - (1/2)*(l+1)^2+(1/2)*l
       + (1/2)*μSTEP*(l+1)^2 - (1/2)*μSTEP*(l+1)
-      )/(σSTEP*σSLOPE) :: Float64
+      ) :: Float64
   else
     return (
       - (l+1)*l*μSTEP+(1/2)*t - (1/2)*N+(1/2)*(N+1)^2 + μSLOPE*(N+1)*μSTEP
@@ -36,7 +36,7 @@ function GMStepSlope(
       + (1/2)*μSTEP*(N+1) + (1/2)*μSTEP*(l+1)^2 - (1/2)*μSTEP*(l+1)
       + μSLOPE*(t+1) + l*μSTEP*μSLOPE - (1/2)*(t+1)^2 + (N+1)*l*μSTEP
       - (N+1)*l+(t+1)*l
-      )/(σSTEP*σSLOPE) :: Float64
+      ) :: Float64
   end
 
   #return 0.0 :: Float64
@@ -57,11 +57,11 @@ function GMStepSpike(
   if j > i
     return (
       i*μSPIKE*μSTEP - (N-i)*(1-μSTEP)*μSPIKE + (1-μSTEP)*μSPIKE + (1-μSPIKE)*(1-μSTEP)
-      )/(σSTEP*σSPIKE)  :: Float64
+      )  :: Float64
   else
     return (
       i*μSPIKE*μSTEP - μSTEP*μSPIKE - μSTEP*(1-μSPIKE) - (N-i)*(1-μSTEP)*μSPIKE
-      )/(σSTEP*σSPIKE)  :: Float64
+      )  :: Float64
   end
 
   return 0.0 :: Float64
@@ -79,13 +79,13 @@ function GMStepSin(
     μSIN::Float64, σSIN::Float64,ω::Float64
     )
 
-  return (μSTEP*μSIN*(t+1)-(1/2)*μSTEP*sin(ω)*cos((t+1)*ω)/(cos(ω)-1)
-        +(1/2)*μSTEP*sin((t+1)*ω)-μSTEP*μSIN+(1/2)*μSTEP*sin(ω)*cos(ω)/(cos(ω)-1)
-        -(1/2)*μSTEP*sin(ω)+(μSTEP*μSIN-μSIN)*(N+1)-(1/2)*(-1+μSTEP)*sin(ω)*cos((N+1)*ω)/(cos(ω)-1)
-        +(-1/2+(1/2)*μSTEP)*sin((N+1)*ω)-(μSTEP*μSIN-μSIN)*(t+1)
-        +(1/2)*(-1+μSTEP)*sin(ω)*cos((t+1)*ω)/(cos(ω)-1)-(-1/2+(1/2)*μSTEP)*sin((t+1)*ω)
-        )/(σSTEP*σSIN
-        ) :: Float64
+  return (
+    μSTEP*μSIN*(t+1)-(1/2)*μSTEP*sin(ω)*cos((t+1)*ω)/(cos(ω)-1)
+    +(1/2)*μSTEP*sin((t+1)*ω)-μSTEP*μSIN+(1/2)*μSTEP*sin(ω)*cos(ω)/(cos(ω)-1)
+    -(1/2)*μSTEP*sin(ω)+(μSTEP*μSIN-μSIN)*(N+1)-(1/2)*(-1+μSTEP)*sin(ω)*cos((N+1)*ω)/(cos(ω)-1)
+    +(-1/2+(1/2)*μSTEP)*sin((N+1)*ω)-(μSTEP*μSIN-μSIN)*(t+1)
+    +(1/2)*(-1+μSTEP)*sin(ω)*cos((t+1)*ω)/(cos(ω)-1)-(-1/2+(1/2)*μSTEP)*sin((t+1)*ω)
+    ) :: Float64
 
   #return GM::Float64
 end
@@ -102,13 +102,15 @@ function GMStepCos(
     μCOS::Float64, σCOS::Float64,ω::Float64
     )
 
-    return (μSTEP*μCOS*(t+1)+(1/2)*μSTEP*cos((t+1)*ω)
-          +(1/2)*μSTEP*sin(ω)*sin((t+1)*ω)/(cos(ω)-1)
-          -μSTEP*μCOS-(1/2)*μSTEP*cos(ω)-(1/2)*μSTEP*sin(ω)^2/(cos(ω)-1)
-          +(μSTEP*μCOS-μCOS)*(N+1)+(-1/2+(1/2)*μSTEP)*cos((N+1)*ω)
-          -(1/2)*(1-μSTEP)*sin(ω)*sin((N+1)*ω)/(cos(ω)-1)
-          -(μSTEP*μCOS-μCOS)*(t+1)-(-1/2+(1/2)*μSTEP)*cos((t+1)*ω)
-          +(1/2)*(1-μSTEP)*sin(ω)*sin((t+1)*ω)/(cos(ω)-1))/(σSTEP*σCOS) :: Float64
+    return (
+    μSTEP*μCOS*(t+1)+(1/2)*μSTEP*cos((t+1)*ω)
+    +(1/2)*μSTEP*sin(ω)*sin((t+1)*ω)/(cos(ω)-1)
+    -μSTEP*μCOS-(1/2)*μSTEP*cos(ω)-(1/2)*μSTEP*sin(ω)^2/(cos(ω)-1)
+    +(μSTEP*μCOS-μCOS)*(N+1)+(-1/2+(1/2)*μSTEP)*cos((N+1)*ω)
+    -(1/2)*(1-μSTEP)*sin(ω)*sin((N+1)*ω)/(cos(ω)-1)
+    -(μSTEP*μCOS-μCOS)*(t+1)-(-1/2+(1/2)*μSTEP)*cos((t+1)*ω)
+    +(1/2)*(1-μSTEP)*sin(ω)*sin((t+1)*ω)/(cos(ω)-1)
+    ) :: Float64
 end
 function GMStepCos(i::Int, j::Int, d, IT)
     return GMStepCos(i, j, IT.obs, d.μ[STEP][i], d.σ[STEP][i], d.μ[COS][j], d.σ[COS][j],d.fc[j]) :: Float64
@@ -130,7 +132,7 @@ function GMSlopeSlope(m::Int, M::Int, N::Int, μm::Float64, σm::Float64, μM::F
     + (1/2)*M*(N+1) - (1/2)*μM*(N+1)^2 - (M+1)*m*M
     - (M+1)*μM*M + (N+1)*m*M + μM*(N+1)*m + (N+1)*μM*M
     + μM*(N+1)*μM + (1/2)*μM*(N+1)
-    )/(σm*σM) :: Float64
+    ) :: Float64
 end
 function GMSlopeSlope(i::Int, j::Int, d, IT)
   	if j>i
@@ -154,14 +156,14 @@ function GMSlopeSpike(
       - μSPIKE*(l+1)*μSLOPE + (1/2)*μSPIKE*(l+1)^2
       - (1/2)*μSPIKE*(l+1) + (p-l-μSLOPE)*μSPIKE
       + (p-l-μSLOPE)*(1-μSPIKE)
-      )/(σSLOPE*σSPIKE) :: Float64
+      ) :: Float64
   else
     return (
       l*μSLOPE*μSPIKE + μSPIKE*(N+1)*l + μSPIKE*(N+1)*μSLOPE
       - (1/2)*μSPIKE*(N+1)^2 + (1/2)*μSPIKE*(N+1) - μSPIKE*(l+1)*l
       - μSPIKE*(l+1)*μSLOPE + (1/2)*μSPIKE*(l+1)^2
       - (1/2)*μSPIKE*(l+1) - μSLOPE*μSPIKE - μSLOPE*(1-μSPIKE)
-      )/(σSLOPE*σSPIKE) :: Float64
+      ) :: Float64
   end
 
   return 0.0::Float64
@@ -179,19 +181,19 @@ function GMSlopeSin(
     μSIN::Float64, σSIN::Float64,ω::Float64
     )
 
-  return (μSLOPE*μSIN*(l+1)-(1/2)*μSLOPE*sin(ω)*cos((l+1)*ω)/(cos(ω)-1)
-        +(1/2)*μSLOPE*sin((l+1)*ω)-μSLOPE*μSIN+(1/2)*μSLOPE*sin(ω)*cos(ω)/(cos(ω)-1)
-        -(1/2)*μSLOPE*sin(ω)+(l*μSIN+(1/2)*μSIN+μSLOPE*μSIN)*(N+1)
-        -(1/2)*μSIN*(N+1)^2+(1/2)*sin(ω)*(N+1)*cos((N+1)*ω)/(cos(ω)-1)-(1/2*(N+1))*sin((N+1)*ω)
-        -(1/2)*(μSLOPE+l)*sin(ω)*cos((N+1)*ω)/(cos(ω)-1)
-        +(1/2)*(l*cos(ω)+μSLOPE*cos(ω)-l-μSLOPE-1)*sin((N+1)*ω)/(cos(ω)-1)
-        -(l*μSIN+(1/2)*μSIN+μSLOPE*μSIN)*(l+1)
-        +(1/2)*μSIN*(l+1)^2-(1/2)*sin(ω)*(l+1)*cos((l+1)*ω)/(cos(ω)-1)
-        +(1/2*(l+1))*sin((l+1)*ω)
-        +(1/2)*(μSLOPE+l)*sin(ω)*cos((l+1)*ω)/(cos(ω)-1)
-        -(1/2)*(l*cos(ω)+μSLOPE*cos(ω)-l-μSLOPE-1)*sin((l+1)*ω)/(cos(ω)-1)
-        )/(σSLOPE*σSIN
-        ) ::Float64
+  return (
+    μSLOPE*μSIN*(l+1)-(1/2)*μSLOPE*sin(ω)*cos((l+1)*ω)/(cos(ω)-1)
+    +(1/2)*μSLOPE*sin((l+1)*ω)-μSLOPE*μSIN+(1/2)*μSLOPE*sin(ω)*cos(ω)/(cos(ω)-1)
+    -(1/2)*μSLOPE*sin(ω)+(l*μSIN+(1/2)*μSIN+μSLOPE*μSIN)*(N+1)
+    -(1/2)*μSIN*(N+1)^2+(1/2)*sin(ω)*(N+1)*cos((N+1)*ω)/(cos(ω)-1)-(1/2*(N+1))*sin((N+1)*ω)
+    -(1/2)*(μSLOPE+l)*sin(ω)*cos((N+1)*ω)/(cos(ω)-1)
+    +(1/2)*(l*cos(ω)+μSLOPE*cos(ω)-l-μSLOPE-1)*sin((N+1)*ω)/(cos(ω)-1)
+`  -(l*μSIN+(1/2)*μSIN+μSLOPE*μSIN)*(l+1)
+    +(1/2)*μSIN*(l+1)^2-(1/2)*sin(ω)*(l+1)*cos((l+1)*ω)/(cos(ω)-1)
+    +(1/2*(l+1))*sin((l+1)*ω)
+    +(1/2)*(μSLOPE+l)*sin(ω)*cos((l+1)*ω)/(cos(ω)-1)
+    -(1/2)*(l*cos(ω)+μSLOPE*cos(ω)-l-μSLOPE-1)*sin((l+1)*ω)/(cos(ω)-1)
+    ) :: Float64
 end
 function GMSlopeSin(i::Int, j::Int, d, IT)
     return GMSlopeSin(i, j, IT.obs, d.μ[SLOPE][i], d.σ[SLOPE][i], d.μ[SIN][j], d.σ[SIN][j], d.fs[j]) :: Float64
@@ -206,7 +208,8 @@ function GMSlopeCos(
     μCOS::Float64, σCOS::Float64, ω::Float64
     )
 
-  return (μSLOPE*μCOS*(l+1)+(1/2)*μSLOPE*cos((l+1)*ω)
+  return (
+    μSLOPE*μCOS*(l+1)+(1/2)*μSLOPE*cos((l+1)*ω)
     +(1/2)*μSLOPE*sin(ω)*sin((l+1)*ω)/(cos(ω)-1)
     -μSLOPE*μCOS-(1/2)*μSLOPE*cos(ω)-(1/2)*μSLOPE*sin(ω)^2/(cos(ω)-1)
     +(μSLOPE*μCOS+l*μCOS+(1/2)*μCOS)*(N+1)
@@ -219,7 +222,7 @@ function GMSlopeCos(
     +(1/2)*sin(ω)*(l+1)*sin((l+1)*ω)/(cos(ω)-1)
     -(1/2)*(l*cos(ω)+μSLOPE*cos(ω)-l-μSLOPE-1)*cos((l+1)*ω)/(cos(ω)-1)
     -(1/2)*(l+μSLOPE)*sin(ω)*sin((l+1)*ω)/(cos(ω)-1)
-    )/(σSLOPE*σCOS) ::Float64
+    ) :: Float64
 end
 function GMSlopeCos(i::Int, j::Int, d, IT)
     return GMSlopeCos(i, j, IT.obs, d.μ[SLOPE][i], d.σ[SLOPE][i], d.μ[COS][j], d.σ[COS][j],d.fc[j]) :: Float64
@@ -234,7 +237,7 @@ function GMSpikeSpike(i::Int, j::Int, N::Int, μSPIKEi::Float64, σSPIKEi::Float
   if i != j
     return (
       (N-2)*μSPIKEi^2 - 2*μSPIKEi*(1-μSPIKEi)
-    )/(σSPIKEi*σSPIKEi) :: Float64
+    ) :: Float64
   else
     return float(N) :: Float64
   end
@@ -254,7 +257,7 @@ function GMSpikeSin(
    return (μSPIKE*μSIN*(N+1)-(1/2)*μSPIKE*sin(ω)*cos((N+1)*ω)/(cos(ω)-1)
         +(1/2)*μSPIKE*sin((N+1)*ω)-μSPIKE*μSIN+(1/2)*μSPIKE*sin(ω)*cos(f[s])/(cos(f[s])-1)
         -(1/2)*μSPIKE*sin(ω)+μSPIKE*(sin(p*ω)-μSIN)+(1-μSPIKE)*(sin(p*ω)-μSIN)
-        )/(σSPIKE*σSIN) :: Float64
+        ) :: Float64
 end
 function GMSpikeSin(i::Int, j::Int, d, IT)
     return GMSpikeSin(i, j, IT.obs, d.μ[SPIKE][i], d.σ[SPIKE][i], d.μ[SIN][j], d.σ[SIN][j], d.fs[j]) :: Float64
@@ -269,10 +272,12 @@ function GMSpikeCos(
     μCOS::Float64, σCOS::Float64, ω::Float64
     )
 
-  return (μSPIKE*μCOS*(N+1)+(1/2)*μSPIKE*cos((N+1)*ω)
+  return (
+    μSPIKE*μCOS*(N+1)+(1/2)*μSPIKE*cos((N+1)*ω)
     +(1/2)*μSPIKE*sin(ω)*sin((N+1)*ω)/(cos(ω)-1)
     -μSPIKE*μCOS-(1/2)*μSPIKE*cos(ω)-(1/2)*μSPIKE*sin(ω)^2/(cos(ω)-1)
-    +μSPIKE*(cos(p*ω)-μCOS)+(1-μSPIKE)*(cos(p*ω)-μCOS))/(σSPIKE*σCOS) :: Float64
+    +μSPIKE*(cos(p*ω)-μCOS)+(1-μSPIKE)*(cos(p*ω)-μCOS)
+    ) :: Float64
 end
 function GMSpikeCos(i::Int, j::Int, d, IT)
     return GMSpikeCos(i, j, IT.obs, d.μ[SPIKE][i], d.σ[SPIKE][i], d.μ[COS][j], d.σ[COS][j],d.fc[j]) :: Float64
@@ -290,24 +295,26 @@ function GMSinSin(i::Int, j::Int, N::Int, μSINi::Float64, σSINi::Float64, μSI
     return float(N) :: Float64
   elseif abs( cos(ωj)-cos(ωi) ) <= 1e-8
     #cancelation that need to happen for finiteness
-    return (μSINj*μSINi*(N+1)
-          -(1/2)*sin((N+1)*ωj)*sin((N+1)*ωi)-(1/2)*μSINj*sin(ωi)*cos((N+1)*ωi)/(-1+cos(ωi))
-          -(1/2)*μSINi*sin(ωj)*cos((N+1)*ωj)/(cos(ωj)-1)+(1/2)*μSINj*sin((N+1)*ωi)
-          +(1/2)*sin((N+1)*ωj)*μSINi-μSINj*μSINi
-          +(1/2)*sin(ωi)*sin(ωj)
-          +(1/2)*μSINj*sin(ωi)*cos(ωi)/(-1+cos(ωi))+(1/2)*μSINi*sin(ωj)*cos(ωj)/(cos(ωj)-1)
-          -(1/2)*μSINj*sin(ωi)-(1/2)*μSINi*sin(ωj)
-          )/(σSINj*σSINi) :: Float64
+    return (
+      μSINj*μSINi*(N+1)
+      -(1/2)*sin((N+1)*ωj)*sin((N+1)*ωi)-(1/2)*μSINj*sin(ωi)*cos((N+1)*ωi)/(-1+cos(ωi))
+      -(1/2)*μSINi*sin(ωj)*cos((N+1)*ωj)/(cos(ωj)-1)+(1/2)*μSINj*sin((N+1)*ωi)
+      +(1/2)*sin((N+1)*ωj)*μSINi-μSINj*μSINi
+      +(1/2)*sin(ωi)*sin(ωj)
+      +(1/2)*μSINj*sin(ωi)*cos(ωi)/(-1+cos(ωi))+(1/2)*μSINi*sin(ωj)*cos(ωj)/(cos(ωj)-1)
+      -(1/2)*μSINj*sin(ωi)-(1/2)*μSINi*sin(ωj)
+      ) :: Float64
   else
-    return (μSINj*μSINi*(N+1)+(1/2)*sin(ωj)*cos((N+1)*ωj)*sin((N+1)*ωi)/(cos(ωj)-cos(ωi))
-          -(1/2)*sin(ωi)*sin((N+1)*ωj)*cos((N+1)*ωi)/(cos(ωj)-cos(ωi))
-          -(1/2)*sin((N+1)*ωj)*sin((N+1)*ωi)-(1/2)*μSINj*sin(ωi)*cos((N+1)*ωi)/(-1+cos(ωi))
-          -(1/2)*μSINi*sin(ωj)*cos((N+1)*ωj)/(cos(ωj)-1)+(1/2)*μSINj*sin((N+1)*ωi)
-          +(1/2)*sin((N+1)*ωj)*μSINi-μSINj*μSINi-(1/2)*sin(ωj)*cos(ωj)*sin(ωi)/(cos(ωj)-cos(ωi))
-          +(1/2)*sin(ωi)*sin(ωj)*cos(ωi)/(cos(ωj)-cos(ωi))+(1/2)*sin(ωi)*sin(ωj)
-          +(1/2)*μSINj*sin(ωi)*cos(ωi)/(-1+cos(ωi))+(1/2)*μSINi*sin(ωj)*cos(ωj)/(cos(ωj)-1)
-          -(1/2)*μSINj*sin(ωi)-(1/2)*μSINi*sin(ωj)
-          )/(σSINj*σSINi) :: Float64
+    return (
+      μSINj*μSINi*(N+1)+(1/2)*sin(ωj)*cos((N+1)*ωj)*sin((N+1)*ωi)/(cos(ωj)-cos(ωi))
+      -(1/2)*sin(ωi)*sin((N+1)*ωj)*cos((N+1)*ωi)/(cos(ωj)-cos(ωi))
+      -(1/2)*sin((N+1)*ωj)*sin((N+1)*ωi)-(1/2)*μSINj*sin(ωi)*cos((N+1)*ωi)/(-1+cos(ωi))
+      -(1/2)*μSINi*sin(ωj)*cos((N+1)*ωj)/(cos(ωj)-1)+(1/2)*μSINj*sin((N+1)*ωi)
+      +(1/2)*sin((N+1)*ωj)*μSINi-μSINj*μSINi-(1/2)*sin(ωj)*cos(ωj)*sin(ωi)/(cos(ωj)-cos(ωi))
+      +(1/2)*sin(ωi)*sin(ωj)*cos(ωi)/(cos(ωj)-cos(ωi))+(1/2)*sin(ωi)*sin(ωj)
+      +(1/2)*μSINj*sin(ωi)*cos(ωi)/(-1+cos(ωi))+(1/2)*μSINi*sin(ωj)*cos(ωj)/(cos(ωj)-1)
+      -(1/2)*μSINj*sin(ωi)-(1/2)*μSINi*sin(ωj)
+      ) :: Float64
   end
 
   return 0.0 :: Float64
@@ -325,10 +332,24 @@ function GMSinCos(
 
   if abs(cos(ωc)-cos(ωs)) >= 1e-5
 
-    return (μSIN*μCOS*(N+1)+(1/2)*sin(ωs)*cos((N+1)*ωs)*cos((N+1)*ωc)/(cos(ωs)-cos(ωc))-(1/2)*sin((N+1)*ωs)*cos((N+1)*ωc)+(1/2)*sin(ωc)*sin((N+1)*ωs)*sin((N+1)*ωc)/(cos(ωs)-cos(ωc))+(1/2)*μSIN*cos((N+1)*ωc)-(1/2)*μCOS*sin(ωs)*cos((N+1)*ωs)/(cos(ωs)-1)+(1/2)*μSIN*sin(ωc)*sin((N+1)*ωc)/(cos(ωc)-1)+(1/2)*sin((N+1)*ωs)*μCOS-μSIN*μCOS-(1/2)*sin(ωs)*cos(ωs)*cos(ωc)/(cos(ωs)-cos(ωc))+(1/2)*sin(ωs)*cos(ωc)-(1/2)*sin(ωc)^2*sin(ωs)/(cos(ωs)-cos(ωc))-(1/2)*μSIN*cos(ωc)+(1/2)*μCOS*sin(ωs)*cos(ωs)/(cos(ωs)-1)-(1/2)*μSIN*sin(ωc)^2/(cos(ωc)-1)-(1/2)*sin(ωs)*μCOS)/(σCOS*σSIN) :: Float64
+    return (
+      μSIN*μCOS*(N+1)+(1/2)*sin(ωs)*cos((N+1)*ωs)*cos((N+1)*ωc)/(cos(ωs)-cos(ωc))
+      -(1/2)*sin((N+1)*ωs)*cos((N+1)*ωc)+(1/2)*sin(ωc)*sin((N+1)*ωs)*sin((N+1)*ωc)/(cos(ωs)-cos(ωc))
+      +(1/2)*μSIN*cos((N+1)*ωc)-(1/2)*μCOS*sin(ωs)*cos((N+1)*ωs)/(cos(ωs)-1)
+      +(1/2)*μSIN*sin(ωc)*sin((N+1)*ωc)/(cos(ωc)-1)+(1/2)*sin((N+1)*ωs)*μCOS-μSIN*μCOS
+      -(1/2)*sin(ωs)*cos(ωs)*cos(ωc)/(cos(ωs)-cos(ωc))+(1/2)*sin(ωs)*cos(ωc)
+      -(1/2)*sin(ωc)^2*sin(ωs)/(cos(ωs)-cos(ωc))-(1/2)*μSIN*cos(ωc)+(1/2)*μCOS*sin(ωs)*cos(ωs)/(cos(ωs)-1)
+      -(1/2)*μSIN*sin(ωc)^2/(cos(ωc)-1)-(1/2)*sin(ωs)*μCOS
+      ) :: Float64
 
   else
-    return (μSIN*μCOS*(N+1)-(1/2)*cos(ωs)*cos((N+1)*ωs)^2/sin(ωs)-(1/2)*sin((N+1)*ωs)*cos((N+1)*ωs)+(1/2)*(cos(ωs)*μCOS+μSIN*sin(ωs)+μCOS)*cos((N+1)*ωs)/sin(ωs)+(1/2)*(-cos(ωs)*μSIN+μCOS*sin(ωs)-μSIN)*sin((N+1)*ωs)/sin(ωs)-μSIN*μCOS+(1/2)*cos(ωs)^3/sin(ωs)+(1/2)*cos(ωs)*sin(ωs)-(1/2)*(cos(ωs)*μCOS+μSIN*sin(ωs)+μCOS)*cos(ωs)/sin(ωs)+(1/2)*cos(ωs)*μSIN-(1/2)*μCOS*sin(ωs)+(1/2)*μSIN)/(σCOS*σSIN) :: Float64
+    return (
+      μSIN*μCOS*(N+1)-(1/2)*cos(ωs)*cos((N+1)*ωs)^2/sin(ωs)-(1/2)*sin((N+1)*ωs)*cos((N+1)*ωs)
+      +(1/2)*(cos(ωs)*μCOS+μSIN*sin(ωs)+μCOS)*cos((N+1)*ωs)/sin(ωs)
+      +(1/2)*(-cos(ωs)*μSIN+μCOS*sin(ωs)-μSIN)*sin((N+1)*ωs)/sin(ωs)-μSIN*μCOS
+      +(1/2)*cos(ωs)^3/sin(ωs)+(1/2)*cos(ωs)*sin(ωs)-(1/2)*(cos(ωs)*μCOS+μSIN*sin(ωs)+μCOS)*cos(ωs)/sin(ωs)
+      +(1/2)*cos(ωs)*μSIN-(1/2)*μCOS*sin(ωs)+(1/2)*μSIN
+      ) :: Float64
 
   end
 
@@ -347,19 +368,30 @@ function GMCosCos(i::Int, j::Int, N::Int, μCOSi::Float64, σCOSi::Float64, μCO
   if i == j
     return float(N)
   elseif abs(cos(ωi)-cos(ωj) ) >= 1e-8
-    return (μCOSj*μCOSi*(N+1)-(1/2)*cos((N+1)*ωj)*cos((N+1)*ωi)+(1/2)*sin(ωi)*cos((N+1)*ωj)*sin((N+1)*ωi)/(cos(ωj)-cos(ωi))-(1/2)*sin(ωj)*sin((N+1)*ωj)*cos((N+1)*ωi)/(cos(ωj)-cos(ωi))+(1/2)*μCOSj*cos((N+1)*ωi)+(1/2)*cos((N+1)*ωj)*μCOSi+(1/2)*μCOSj*sin(ωi)*sin((N+1)*ωi)/(-1+cos(ωi))+(1/2)*μCOSi*sin(ωj)*sin((N+1)*ωj)/(cos(ωj)-1)-μCOSj*μCOSi+(1/2)*cos(ωj)*cos(ωi)-(1/2)*sin(ωi)^2*cos(ωj)/(cos(ωj)-cos(ωi))+(1/2)*sin(ωj)^2*cos(ωi)/(cos(ωj)-cos(ωi))-(1/2)*μCOSj*cos(ωi)-(1/2)*cos(ωj)*μCOSi-(1/2)*μCOSj*sin(ωi)^2/(-1+cos(ωi))-(1/2)*μCOSi*sin(ωj)^2/(cos(ωj)-1))/(σCOSj*σCOSi)
+    return (
+      μCOSj*μCOSi*(N+1)-(1/2)*cos((N+1)*ωj)*cos((N+1)*ωi)
+      +(1/2)*sin(ωi)*cos((N+1)*ωj)*sin((N+1)*ωi)/(cos(ωj)-cos(ωi))
+      -(1/2)*sin(ωj)*sin((N+1)*ωj)*cos((N+1)*ωi)/(cos(ωj)-cos(ωi))
+      +(1/2)*μCOSj*cos((N+1)*ωi)+(1/2)*cos((N+1)*ωj)*μCOSi
+      +(1/2)*μCOSj*sin(ωi)*sin((N+1)*ωi)/(-1+cos(ωi))
+      +(1/2)*μCOSi*sin(ωj)*sin((N+1)*ωj)/(cos(ωj)-1)-μCOSj*μCOSi+(1/2)*cos(ωj)*cos(ωi)
+      -(1/2)*sin(ωi)^2*cos(ωj)/(cos(ωj)-cos(ωi))
+      +(1/2)*sin(ωj)^2*cos(ωi)/(cos(ωj)-cos(ωi))-(1/2)*μCOSj*cos(ωi)
+      -(1/2)*cos(ωj)*μCOSi-(1/2)*μCOSj*sin(ωi)^2/(-1+cos(ωi))-(1/2)*μCOSi*sin(ωj)^2/(cos(ωj)-1)
+      ) :: Float64
   else
-    return (μCOSj*μCOSi*(N+1)-(1/2)*cos((N+1)*ωj)*cos((N+1)*ωi)
+    return (
+      μCOSj*μCOSi*(N+1)-(1/2)*cos((N+1)*ωj)*cos((N+1)*ωi)
       +(1/2)*μCOSj*cos((N+1)*ωi)+(1/2)*cos((N+1)*ωj)*μCOSi
       +(1/2)*μCOSj*sin(ωi)*sin((N+1)*ωi)/(-1+cos(ωi))
       +(1/2)*μCOSi*sin(ωj)*sin((N+1)*ωj)/(cos(ωj)-1)
       -μCOSj*μCOSi+(1/2)*cos(ωj)*cos(ωi)
       -(1/2)*μCOSj*cos(ωi)-(1/2)*cos(ωj)*μCOSi
       -(1/2)*μCOSj*sin(ωi)^2/(-1+cos(ωi))
-      -(1/2)*μCOSi*sin(ωj)^2/(cos(ωj)-1))/(σCOSj*σCOSi)
+      -(1/2)*μCOSi*sin(ωj)^2/(cos(ωj)-1)
+      ) :: Float64
   end
   return 0.0::Float64
-
 end
 function GMCosCos(i::Int, j::Int, d, IT)
     return GMCosCos(i, j, IT.obs, d.μ[COS][i], d.σ[COS][i],d.μ[COS][j], d.σ[COS][j],d.fc[i],d.fc[j]) :: Float64
@@ -414,7 +446,7 @@ function GM2(c1,c2,i::Int, j::Int, d, IT)
   elseif c1 == SPIKE && c2 == SPIKE
     return GMSpikeSpike(i::Int, j::Int, d, IT) :: Float64
   elseif c1 == SPIKE && c2 == SLOPE
-    return GMSpikeSlope(i::Int, j::Int, d, IT):: Float64 
+    return GMSpikeSlope(i::Int, j::Int, d, IT):: Float64
   elseif c1 == SLOPE && c2 == SPIKE
     return GMSlopeSpike(i::Int, j::Int, d, IT) :: Float64
   elseif c1 == SPIKE && c2 == SIN
@@ -422,7 +454,7 @@ function GM2(c1,c2,i::Int, j::Int, d, IT)
   elseif c1 == SIN && c2 == SPIKE
     return GMSinSpike(i::Int, j::Int, d, IT) :: Float64
   elseif c1 == SPIKE && c2 == COS
-    return GMSpikeCos(i::Int, j::Int, d, IT):: Float64 
+    return GMSpikeCos(i::Int, j::Int, d, IT):: Float64
   elseif c1 == COS && c2 == SPIKE
     return GMCosSpike(i::Int, j::Int, d, IT) :: Float64
   elseif c1 == SLOPE && c2 == SLOPE
